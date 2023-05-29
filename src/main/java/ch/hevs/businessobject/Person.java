c0589cf12d40f20e6758442fc328777b5e191977
Elias <borrajo.elias@gmail.com>
2023-05-29 16:05:53 +0200

    Business Objects Relations & Debug

--- src/main/java/ch/hevs/businessobject/Person.java
+++ src/main/java/ch/hevs/businessobject/Person.java
@@ -3,9 +3,9 @@ package ch.hevs.businessobject;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity     // TODO : CHANGER TYPE HERITAGE
+@Entity
 @Inheritance ( strategy = InheritanceType.TABLE_PER_CLASS )
-public abstract class Person
+public abstract class Person  // TODO LAISSER ABSTRACT ? Ou ça casse tout ?
 {
     // A T T R I B U T S
     @Id
@@ -19,4 +19,64 @@ public abstract class Person
     @Embedded
     private Country country;
 
+    // C O N S T R U C T O R S
+    public Person() {
+    }
+    public Person(String firstname, String lastname, Date birthdate, Country country) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.country = country;
+    }
+
+    // G E T T E R S   &   S E T T E R S
+    // id
+    public Long getId()
+    {
+        return id;
+    }
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    // firstname
+    public String getFirstname()
+    {
+        return firstname;
+    }
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    // lastname
+    public String getLastname()
+    {
+        return lastname;
+    }
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+
+    // birthdate
+    public Date getBirthdate()
+    {
+        return birthdate;
+    }
+    public void setBirthdate(Date birthdate)
+    {
+        this.birthdate = birthdate;
+    }
+
+    // country
+    public Country getCountry()
+    {
+        return country;
+    }
+    public void setCountry(Country country)
+    {
+        this.country = country;
+    }
 }
--- src/main/java/ch/hevs/businessobject/Player.java
+++ src/main/java/ch/hevs/businessobject/Player.java
@@ -20,14 +20,63 @@ public class Player extends Person
 
     // C O N S T R U C T O R
     public Player() {
+        super();
     }
-    public Player(String position, int number, boolean isRightFooted, double height, double weight /*, Club playsForClub*/)
+    public Player(String firstname, String lastname,
+                  Date birthdate, Country country,
+                  String position, int number,
+                  boolean isRightFooted, double height,
+                  double weight /*, Club playsForClub*/)
     {
+        super(firstname, lastname, birthdate, country);
         this.position = position;
         this.number = number;
         this.isRightFooted = isRightFooted;
         this.height = height;
         this.weight = weight;
-        //this.playsForClub = playsForClub; // TODO METTRE ?
+        //this.playsForClub = playsForClub; // TODO METTRE ? JE PENSE PAS CAR MIS AVEC METHODES DEPUIS GRAPHIQUE
     }
+
+    // G E T T E R S   &   S E T T E R S
+    // position
+    public String getPosition() {return position;}
+    public void setPosition(String position) {this.position = position;}
+
+    // number
+    public int getNumber() {return number;}
+    public void setNumber(int number) {this.number = number;}
+
+    // isRightFooted
+    public boolean isRightFooted() {return isRightFooted;}
+    public void setRightFooted(boolean rightFooted) {isRightFooted = rightFooted;}
+
+    // height
+    public double getHeight() {return height;}
+    public void setHeight(double height) {this.height = height;}
+
+    // weight
+    public double getWeight() {return weight;}
+    public void setWeight(double weight) {this.weight = weight;}
+
+    // playsForClub
+    public Club getPlaysForClub() {return playsForClub;}
+    public void setPlaysForClub(Club playsForClub) {this.playsForClub = playsForClub;}
+
+
+
+    // C O N V E N I E N C E   M E T H O D S
+    @Override
+    public String toString() {
+        return "Player{" +
+                "position='" + position + '\'' +
+                ", number=" + number +
+                ", isRightFooted=" + isRightFooted +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", playsForClub=" + playsForClub +
+                '}';
+    }
+
+
+
 }
